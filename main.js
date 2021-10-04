5ccc48ee3672915ec27d2a569ff9b83fe4102126
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2021-10-04 14:54:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+prediction_1="";
+
+
 Webcam.set({
     width:350,
     height:300,
@@ -6,31 +9,54 @@ Webcam.set({
 });
 
 camera=document.getElementById("camera");
-
-Webcam.attach("#camera")
+Webcam.attach(camera)
 
 function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML="<img id='snap_shot' src='"+data_uri+"'>"
-    });
+  Webcam.snap(function (data_uri){
+document.getElementById("result").innerHTML="<img id='captured_img' src='"+data_uri +"'>";
+  });
+  
 }
-    console.log("ml5 version="+ml5.version);
- classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/y1kR7Sqfj/model.json",modelLoaded);
-function modelLoaded(){
-    console.log("model has been loaded");
-}   
+
+console.log("ml5 version is ",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/tVLswE2WL/model.json",model_loaded);
+
+function model_loaded(){
+    console.log("model loaded sucessfully")
+}
+
 function check(){
-    img=document.getElementById('snap_shot');
-    classifier.classify(img,gotResult);
+  img=document.getElementById('captured_img');
+ classifier.classify(img,gotResult);
 }
- function gotResult(error,result){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(result);
-        document.getElementById("object_name").innerHTML=result[0].label;
-        percentage=result[0].confidence*100;
-        document.getElementById("object_accuracy").innerHTML=percentage.toFixed(2) +" %";
-    }
-     
- }
+
+function gotResult(error,results){
+if(error){
+  console.log(error);
+}
+else{
+  console.log(results);
+  document.getElementById("result_emotion_name").innerHTML=results[0].label;
+  prediction_1=results[0].label;
+
+  speak();
+
+  if(results[0].label=="thumbs up"){
+   document.getElementById("result_emoji1").innerHTML="&#128077;";
+  }
+  if(results[0].label=="victory"){
+    document.getElementById("result_emoji1").innerHTML="&#9996;";
+   }
+   if(results[0].label=="amazing"){
+    document.getElementById("result_emoji1").innerHTML="&#128076";
+   }
+   if(results[0].label=="horns"){
+    document.getElementById("result_emoji1").innerHTML="&#129304;";
+   }
+   if(results[0].label=="hands with fingers")
+   {
+    document.getElementById("result_emoji1").innerHTML="&#128400;";
+   }
+  }
+
